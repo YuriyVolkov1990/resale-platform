9a899ce9c81034c8963ceb45fae5ec01d2a718e2
Yuriy <new99kpicem@gmail.com>
2023-12-06 22:44:15 +0300

    stepOne_1

--- src/main/java/ru/skypro/homework/dto/UserDto.java
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -1,4 +1,18 @@
 package ru.skypro.homework.dto;
 
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Data
 public class UserDto {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private RoleDto role;
+    private String image;
 }
