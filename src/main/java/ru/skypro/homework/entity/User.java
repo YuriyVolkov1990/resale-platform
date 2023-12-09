6b745ff04dc40d7e291b9a62cb87adafa1cb4be0
Yuriy <new99kpicem@gmail.com>
2023-12-09 20:45:33 +0300

    stepOne_1

--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -1,5 +1,13 @@
 package ru.skypro.homework.entity;
 
-public class User {
+import ru.skypro.homework.dto.Role;
 
+public class User {
+    private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private String image;
 }
