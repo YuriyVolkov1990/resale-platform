faac6c4d5a372f92b5afac34bd809bfddb698119
Yuriy <new99kpicem@gmail.com>
2023-12-13 13:29:08 +0300

    stepOne_1

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -1,4 +1,22 @@
 package ru.skypro.homework.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.CommentsDto;
+import ru.skypro.homework.service.CommentService;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/ads")
 public class CommentController {
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/{id}/comments")
+    public ResponseEntity<CommentsDto> getComments(@PathVariable Integer id) {
+        return ResponseEntity.ok(commentService.getCommentsByAd(id));
+    }
 }
--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -1,4 +1,7 @@
 package ru.skypro.homework.service;
 
+import ru.skypro.homework.dto.CommentsDto;
+
 public interface CommentService {
+    public CommentsDto getCommentsByAd(Integer adId);
 }
