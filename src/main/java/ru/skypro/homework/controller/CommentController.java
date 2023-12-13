ab637924e08bdbc6f9a2c2428a7c46be5644bfa8
Yuriy <new99kpicem@gmail.com>
2023-12-13 13:50:11 +0300

    stepOne_1

--- src/main/java/ru/skypro/homework/controller/CommentController.java
+++ src/main/java/ru/skypro/homework/controller/CommentController.java
@@ -1,7 +1,9 @@
 package ru.skypro.homework.controller;
 
+import liquibase.pro.packaged.R;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.CommentDto;
 import ru.skypro.homework.dto.CommentsDto;
 import ru.skypro.homework.service.CommentService;
 
@@ -9,7 +11,7 @@ import ru.skypro.homework.service.CommentService;
 @RestController
 @RequestMapping("/ads")
 public class CommentController {
-    private CommentService commentService;
+    private final CommentService commentService;
 
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
@@ -18,5 +20,20 @@ public class CommentController {
     @GetMapping("/{id}/comments")
     public ResponseEntity<CommentsDto> getComments(@PathVariable Integer id) {
         return ResponseEntity.ok(commentService.getCommentsByAd(id));
+    }
+
+    @PostMapping("/{id}/comments")
+    public ResponseEntity<CommentDto> getComment(@PathVariable Integer id) {
+        return ResponseEntity.ok(commentService.postCommentToAd(id));
+    }
+
+    @DeleteMapping("/{adId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> deleteComment(@PathVariable Integer adId, @PathVariable Integer id) {
+        return ResponseEntity.ok(commentService.deleteCommentAtAd(adId, id));
+    }
+
+    @PatchMapping("/{adId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> patchComment(@PathVariable Integer adId, @PathVariable Integer id) {
+        return ResponseEntity.ok(commentService.patchCommentAtAd(adId, id));
     }
 }
--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -1,7 +1,14 @@
 package ru.skypro.homework.service;
 
+import ru.skypro.homework.dto.CommentDto;
 import ru.skypro.homework.dto.CommentsDto;
 
 public interface CommentService {
     public CommentsDto getCommentsByAd(Integer adId);
+
+    public CommentDto postCommentToAd(Integer adId);
+
+    public CommentDto deleteCommentAtAd(Integer adId, Integer id);
+
+    public CommentDto patchCommentAtAd(Integer adId, Integer id);
 }
