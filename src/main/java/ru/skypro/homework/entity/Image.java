1aa36bf3d4eb9ff7691d5d8cfd043a11e53ce9fa
Yuriy <new99kpicem@gmail.com>
2023-12-16 17:53:57 +0300

    stepOne_1

--- src/main/java/ru/skypro/homework/entity/Image.java
+++ src/main/java/ru/skypro/homework/entity/Image.java
@@ -4,4 +4,17 @@ import javax.persistence.Entity;
 
 @Entity
 public class Image {
+    private String path;
+
+    public Image(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
