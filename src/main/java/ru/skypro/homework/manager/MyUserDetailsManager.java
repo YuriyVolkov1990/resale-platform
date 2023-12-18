331d2f9c0c5df9c3f6434e4803456d36e777cb21
Yuriy <new99kpicem@gmail.com>
2023-12-18 22:27:37 +0300

    stepOne_3

--- src/main/java/ru/skypro/homework/manager/MyUserDetailsManager.java
+++ src/main/java/ru/skypro/homework/manager/MyUserDetailsManager.java
@@ -1,50 +1,50 @@
-package ru.skypro.homework.manager;
-
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.stereotype.Service;
-import ru.skypro.homework.entity.User;
-import ru.skypro.homework.repository.UserRepository;
-@Service
-public class MyUserDetailsManager implements UserDetailsManager {
-    private final UserRepository userRepository;
-
-    public MyUserDetailsManager(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public void createUser(UserDetails user) {
-        User newUser = new User();
-        newUser.setEmail(user.getUsername());
-        newUser.setPassword(user.getPassword());
-        userRepository.save(newUser);
-    }
-
-    @Override
-    public void updateUser(UserDetails user) {
-
-    }
-
-    @Override
-    public void deleteUser(String username) {
-
-    }
-
-    @Override
-    public void changePassword(String oldPassword, String newPassword) {
-
-    }
-
-    @Override
-    public boolean userExists(String username) {
-        return false;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findUserByEmail(email);
-    }
-}
+//package ru.skypro.homework.manager;
+//
+//
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.stereotype.Service;
+//import ru.skypro.homework.entity.User;
+//import ru.skypro.homework.repository.UserRepository;
+//@Service
+//public class MyUserDetailsManager implements UserDetailsManager {
+//    private final UserRepository userRepository;
+//
+//    public MyUserDetailsManager(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public void createUser(UserDetails user) {
+//        User newUser = new User();
+//        newUser.setEmail(user.getUsername());
+//        newUser.setPassword(user.getPassword());
+//        userRepository.save(newUser);
+//    }
+//
+//    @Override
+//    public void updateUser(UserDetails user) {
+//
+//    }
+//
+//    @Override
+//    public void deleteUser(String username) {
+//
+//    }
+//
+//    @Override
+//    public void changePassword(String oldPassword, String newPassword) {
+//
+//    }
+//
+//    @Override
+//    public boolean userExists(String username) {
+//        return false;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        return userRepository.findUserByEmail(email);
+//    }
+//}
